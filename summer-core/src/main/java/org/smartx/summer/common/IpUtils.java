a91970618497ec35dab3b54aef890f00366e1a6b
kext <luffyke@gmail.com>
2017-10-22 18:05:22 +0800

    define constants

--- summer-core/src/main/java/org/smartx/summer/common/IpUtils.java
+++ summer-core/src/main/java/org/smartx/summer/common/IpUtils.java
@@ -7,18 +7,20 @@ import javax.servlet.http.HttpServletRequest;
  */
 public final class IpUtils {
 
+    private static final String UNKNOWN = "unknown";
+
     public static String getRemoteIP(HttpServletRequest request) {
         String ip = request.getHeader("X-Real-IP");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
             ip = request.getHeader("x-forwarded-for");
         }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
         }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
             ip = request.getHeader("WL-Proxy-Client-IP");
         }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
         }
         return ip;
